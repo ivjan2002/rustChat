17f39a9a228f64689feb8ef59b57abe5c600867e
Ivana <ivanajankovic1302@gmail.com>
2025-03-09 20:42:14 +0100

    css

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,19 +6,43 @@ fn App()->Html{
     let messages=(*messages_handle).clone();
     let ws=use_websocket("ws://127.0.0.1:8000".to_string())
     let mut cloned_messages=messages.clone();
-    use_effect_with(ws.message, move |ws_message| {
+    let new_message_handle=use_state(String::default);
+    use_effect_with(ws.message.clone(), move |ws_message| {
         if let Some(ws_nsg)=&**ws_message{
             cloned_messages.push(ws_nsg.clone());
             messages_handle.set(cloned_messages);
         }
     })
+
+    let on_message_change=Callback::from|e::Event|{
+        let target=e.target_dyn_into::<HtmlTextAreaElement>()
+        if let Some(textarea)=target{
+            new_message_handle.set(textarea.value());
+        }
+    }
+
+    let cloned_new_message_handle=new_message_handle.clone();
+    let on_button_click=Callback::from{move | :MouseEvent |{
+        cloned_ws.send(cloned_new_message.clone());
+        cloned_new_message_handleS.set("".to_string());
+    }}
     html!{
+        <div class="container">
+        <div class="row">
         <ul id="chat">
         {
-            message.iter(),map(|m| html! {<li>{m}</li>}).collect::<Html>()
+            message.iter(),map(|m| html! {<li class="list_group_item">{m}</li>}).collect::<Html>()
         }
         
         </ul>
+        </div>
+        <div class="row">
+        <div class="input_group">
+        <textarea class="form_control" onChange={on_message_change} value={new_message}></textarea>
+        <button type="submit" onClick={on_button_click}>Send</button>
+        </div>
+        </div>
+        <div/>
     }
     
 }
