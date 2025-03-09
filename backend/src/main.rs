5930bca9c3b0ae709aed46cf321ca74f460ab19f
Ivana <ivanajankovic1302@gmail.com>
2025-03-09 10:17:06 +0100

    initial commit

---
+++ backend/src/main.rs
@@ -0,0 +1,20 @@
+
+#[rocket::get("/")]
+fn chat(ws:WebSocket)->Channel<'static>{
+    ws.channel(move|mut stream|Box::pin(async move
+        {while let Some[message]=stream.next.await
+            {let_=stream.send(message?).await;
+            }Ok(())
+        }))
+}
+
+
+#[rocket::main]
+async fn main() {
+    let _=rocket::build()
+    .mount("/",rocket::routes![
+        chat
+    ])
+    .launch()
+    .await;
+}
